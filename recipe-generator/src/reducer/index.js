03f971c7861dd20944cb422df2a368292d084e23
Kat <kyevsukov@gmail.com>
2021-10-06 18:47:05 -0400

    store is set up

--- recipe-generator/src/reducer/index.js
+++ recipe-generator/src/reducer/index.js
@@ -1,9 +1,18 @@
 const initialState = {
-    meal: {
-         recipeName: "",
-        recipeImg: "",
-        recipeInst: ""
-    },
-    isFetching: false,
-    errorMessage: ""
-}
+  meal: {
+    recipeName: "",
+    recipeImg: "",
+    recipeInst: "",
+  },
+  isFetching: false,
+  errorMessage: "",
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
